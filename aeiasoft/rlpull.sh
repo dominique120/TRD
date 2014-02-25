1cadcbb743163cf9155c3ce8c7e1623bb1cfc0a7
dominique120@live.com <dominique120@live.com>
2014-02-25 13:32:28 -0500

    ne script.

---
+++ aeiasoft/rlpull.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script by Dominique Verellen
+
+cd /home
+git pull git@github.com:PrinterLUA/FORGOTTENSERVER-ORTS.git
+cp /home/myfiles/groups.xml /home/FORGOTTENSERVER-ORTS/XML/groups.xml
